4015e43ae470377abb900f1aa57bbf0c6d4e4ee9
Student <example@example.com>
2020-01-28 19:35:27 -0800

    start calculator project

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+var div = function(number1, number2) {
+    return number1 / number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = div(number1, number2);
+alert(result);
+
+
